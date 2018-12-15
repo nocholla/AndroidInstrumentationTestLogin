f85a4cc22e16e459382b987d34b7d4b9679b2fa1
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2018-12-14 16:58:25 -0800

    First commit

---
+++ app/src/main/java/com/nocholla/instrumentationtestlogin/MainActivity.java
@@ -0,0 +1,83 @@
+package com.nocholla.instrumentationtestlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    // ButterKnife
+    @BindView(R.id.username)
+    EditText inputUsername;
+    @BindView(R.id.password)
+    EditText inputPassword;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ButterKnife
+        ButterKnife.bind(this);
+
+        // Login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = inputUsername.getText().toString();
+                Log.d("DEBUG USERNAME", username);
+
+                final String password = inputPassword.getText().toString();
+                Log.d("DEBUG PASSWORD", password);
+
+                // Username = edureka & Password = edureka123" Validation
+                if(username.matches("edureka") && password.matches("edureka123\"")) {
+                    // Toast On Success
+                    Toast.makeText(MainActivity.this, getString(R.string.toast_success), Toast.LENGTH_SHORT).show();
+
+                    // Disable Button On Success
+                    //btnLogin.setEnabled(false);
+                }
+
+                // Wrong Username
+                if(!username.matches("edureka")) {
+                    inputUsername.setError(getString(R.string.error_wrong_username));
+                }
+
+                // Wrong Password
+                if(!password.matches("edureka123\"")) {
+                    inputPassword.setError(getString(R.string.error_wrong_password));
+                }
+
+                // Username Empty Validation
+                if (TextUtils.isEmpty(username)) {
+                    inputUsername.setError(getString(R.string.error_enter_username));
+
+                    return;
+                }
+
+                // Password Empty Validation
+                if (TextUtils.isEmpty(password)) {
+                    inputPassword.setError(getString(R.string.error_enter_password));
+
+                    return;
+                }
+            }
+        });
+
+    }
+}
